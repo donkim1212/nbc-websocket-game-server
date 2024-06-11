f0f51fd92da537ea282128009475e0ba380e90f4
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 12:35:05 +0900

    Update: add 'itemScoreHandler' to the mapping as '15'

--- src/handlers/handler-mapping.js
+++ src/handlers/handler-mapping.js
@@ -1,10 +1,12 @@
 import { moveStageHandler } from "./stage.handler.js";
 import { gameStart, gameEnd } from "./game.handler.js";
+import { itemScoreHandler } from "./item-score.handler.js";
 
 const handlerMappings = {
   2: gameStart,
   3: gameEnd,
   11: moveStageHandler,
+  15: itemScoreHandler,
 };
 
 export default handlerMappings;
