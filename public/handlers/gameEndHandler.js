6b468b63b436b335e9bd64d9e18a47b6d8c65f41
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 16:55:42 +0900

    Update: rename key scorePerSecond -> scoresPerSecond Rename: gameEndHandler -> gameHandler

--- public/handlers/gameEndHandler.js
+++
@@ -1,9 +0,0 @@
-import { score as gameScore } from "../index.js";
-
-const gameEndHandler = (payload, score) => {
-  const { score: finalScore } = payload;
-  gameScore.setScore(finalScore);
-  gameScore.setHighScore();
-};
-
-export default gameEndHandler;
