12a03cfd5543f0e198b4ab2130c70e352eb84910
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 12:58:27 +0900

    Add: itemScoreHandler that adds received itemScore to the current game score

---
+++ public/handlers/itemScoreHandler.js
@@ -0,0 +1,8 @@
+import { score as gameScore } from "../index.js";
+
+const itemScoreHandler = (payload, score) => {
+  const { score: itemScore } = payload;
+  gameScore.addScore(itemScore);
+};
+
+export default itemScoreHandler;
