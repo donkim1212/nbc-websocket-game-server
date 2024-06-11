f034ac1bd3176ac7f679d636ea4ad9ad93f8b54b
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 12:57:11 +0900

    Add: handleResponse func. that passes data to responsible handler

---
+++ public/handlers/helper.js
@@ -0,0 +1,10 @@
+import { handlerMapping, targetMappings } from "./handlerMapping.js";
+
+export const handleResponse = (data) => {
+  const { handlerId, payload } = data;
+  const handler = handlerMapping[handlerId];
+  if (!handler) console.error(`No such handler found: ${handlerId}`);
+  // const target = targetMappings[handlerId];
+  // if (!target) console.error(`No handler target found: ${handlerId}`);
+  handler(payload);
+};
