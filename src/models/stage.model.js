33d068e2f4e6ae6aaac3b92c3bc15ef291b183bd
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 16:16:10 +0900

    Update: 'setStage' now saves previous score to the user's stage info for verification

--- src/models/stage.model.js
+++ src/models/stage.model.js
@@ -8,8 +8,8 @@ export const getStage = (uuid) => {
   return stages[uuid];
 };
 
-export const setStage = (uuid, id, timestamp) => {
-  return stages[uuid].push({ id, timestamp });
+export const setStage = (uuid, id, timestamp, prevScore) => {
+  return stages[uuid].push({ id, timestamp, prevScore });
 };
 
 export const clearStage = (uuid) => {
