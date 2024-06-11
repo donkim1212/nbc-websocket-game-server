f42cc1b46f2b7c7f8bce7f470aba38498d116987
Dongkyun Kim <donkim0122@gmail.com>
2024-06-11 16:54:50 +0900

    Update: change import name as the file renamed Fix: incorrect handler number assigned for gameEndHandler Update: add gameStartHandler and stageHandler

--- public/handlers/handlerMapping.js
+++ public/handlers/handlerMapping.js
@@ -1,15 +1,18 @@
 import itemScoreHandler from "./itemScoreHandler.js";
 // import { score } from "../index.js";
-import gameEndHandler from "./gameEndHandler.js";
+import { gameStartHandler, gameEndHandler } from "./gameHandler.js";
+import stageHandler from "./stageHandler.js";
 
 const handlerMapping = {
-  2: gameEndHandler,
+  2: gameStartHandler,
+  3: gameEndHandler,
+  11: stageHandler,
   15: itemScoreHandler,
 };
 
-const targetMappings = {
+const targetMapping = {
   //   2: score,
   //   15: score,
 };
 
-export { handlerMapping, targetMappings };
+export { handlerMapping, targetMapping };
