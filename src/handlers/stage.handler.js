2d60d3ce74d170b4d278f51241536914e110c5b1
Dongkyun Kim <donkim0122@gmail.com>
2024-06-12 16:09:38 +0900

    Update: replace next stage verification with stageVerification from gameStateVerifier

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -4,17 +4,13 @@ import gsv from "../libs/verifiers/game-state.verifier.js";
 
 export const moveStageHandler = (userId, payload) => {
   // 유저의 현재 스테이지 정보 검증
-  const currentStage = gsv.currentStageVerification(userId, payload.currentStage);
+  const currentStage = gsv.userCurrentStageVerification(userId, payload.currentStage);
 
   // 현재 점수 검증
   const deltaScore = gsv.scoreVerification(userId, currentStage, payload.score);
 
   // advance stage
-  const { stages } = getGameAssets();
   const nextStage = gsv.stageVerification(currentStage.id + 1);
-  if (!stages.data.some((stage) => stage.id === nextStage.id)) {
-    return { status: "fail", message: "Target stage not found." };
-  }
 
   setStage(userId, nextStage.id, Date.now(), deltaScore);
 
