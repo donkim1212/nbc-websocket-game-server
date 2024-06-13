caf6bb89a305b184cbc16c352930456f2a0341eb
Dongkyun Kim <donkim0122@gmail.com>
2024-06-14 01:04:18 +0900

    Feat: 'checkHighscore' updates highscore, 'getHighscore' retrieves highscore

--- src/libs/game-state-manager.js
+++ src/libs/game-state-manager.js
@@ -1,6 +1,10 @@
 import { getGameAssets, getUnlockedItemIdByStageId } from "../init/assets.js";
+import redisClient from "../init/redis.connect.js";
 import { initUserItem, removeUserItemData } from "../models/item.model.js";
 import { stageModelRedis as stageModel } from "../models/stage.model.js";
+
+const HIGHSCORE_KEY = "highscore";
+const HIGHSCORE_USER_KEY = "highscore_user";
 
 const reinitialize = async (userId, timestamp) => {
   const { stages } = getGameAssets();
@@ -31,10 +35,29 @@ const clearGameData = async (userId) => {
   stageModel.removeStage(userId);
 };
 
-const checkHighscore = (userId, score) => {
+const checkHighscore = async (userId, score) => {
   // checks highscore list and add new score if necessary
-  // return highscore
-  return score; // temp
+  const highscore = await redisClient.hVals(HIGHSCORE_KEY);
+  // const highscoreUserId = await redisClient.hKeys(HIGHSCORE_KEY);
+  // console.log("==========", highscore);
+  if (highscore.length === 0 || score >= +highscore[0]) {
+    await redisClient.hSet(HIGHSCORE_KEY, "score", "" + score);
+    await redisClient.hSet(HIGHSCORE_USER_KEY, "user_id", "" + userId);
+    console.log("=======new highscore===", score);
+    return true;
+  }
+  return false; // temp
 };
 
-export { reinitialize, clearGameData, checkHighscore };
+const getHighscore = async () => {
+  // await redisClient.hGetAll(HIGHSCORE_KEY);
+  const highscore = await redisClient.hVals(HIGHSCORE_KEY);
+  const userId = await redisClient.hVals(HIGHSCORE_USER_KEY);
+  console.log(userId, highscore);
+  return {
+    userId: userId[0],
+    highscore: highscore[0],
+  };
+};
+
+export { reinitialize, clearGameData, checkHighscore, getHighscore };
