2c54a3a345b9d0ab1279c518530d2f603d91d921
Dongkyun Kim <donkim0122@gmail.com>
2024-06-14 01:15:10 +0900

    Update: add try/catch on errorHandler in case unexpected error occurs inside the handler

--- src/handlers/error.handler.js
+++ src/handlers/error.handler.js
@@ -2,15 +2,20 @@ import { findUserBySocketId } from "../models/user.model.js";
 import { clearGameData } from "../libs/game-state-manager.js";
 
 const errorHandler = async (err, socket) => {
-  console.error(err);
+  try {
+    console.error(err);
 
-  // delete user's game data on error
-  clearGameData(findUserBySocketId(socket.id).userId);
+    // delete user's game data on error
+    clearGameData(findUserBySocketId(socket.id).userId);
 
-  socket.emit("response", { status: "fail", message: err.message });
-  socket.emit("response", { status: "fail", message: "You are disconnected from the server." });
+    socket.emit("response", { status: "fail", message: err.message });
+    socket.emit("response", { status: "fail", message: "You are disconnected from the server." });
 
-  socket.disconnect(true);
+    socket.disconnect(true);
+  } catch (err) {
+    console.error(err);
+    socket.disconnect(true);
+  }
 };
 
 export default errorHandler;
