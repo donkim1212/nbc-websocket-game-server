de082c908f9e50d9a51cdfc24a3ae40e7f70f3b5
Dongkyun Kim <donkim0122@gmail.com>
2024-06-13 20:05:31 +0900

    Update: add async/await keyword

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -1,10 +1,11 @@
 import { getUnlockedItemIdByStageId } from "../init/assets.js";
-import { setStage } from "../models/stage.model.js";
+import { stageModelRedis as stageModel } from "../models/stage.model.js";
 import gsv from "../libs/game-state-verifier.js";
+import redisClient from "../init/redis.connect.js";
 
-export const moveStageHandler = (userId, payload) => {
+export const moveStageHandler = async (userId, payload) => {
   // 유저의 현재 스테이지 정보 검증
-  const currentStage = gsv.userCurrentStageVerification(userId, payload.currentStage);
+  const currentStage = await gsv.userCurrentStageVerification(userId, payload.currentStage);
 
   // 현재 점수 검증
   const deltaScore = gsv.scoreVerification(userId, currentStage, payload.score);
@@ -12,9 +13,14 @@ export const moveStageHandler = (userId, payload) => {
   // advance stage
   const nextStage = gsv.stageVerification(currentStage.id + 1);
 
-  setStage(userId, nextStage.id, Date.now(), deltaScore);
+  await stageModel.setStage(userId, nextStage.id, Date.now(), deltaScore);
 
   const unlockedId = getUnlockedItemIdByStageId(nextStage.id);
+
+  // await redisClient.set("key", "value");
+  // const value = await redisClient.get("key");
+  // console.log(value);
+  // await redisClient.disconnect();
 
   return {
     status: "success",
