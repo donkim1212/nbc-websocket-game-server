3f430dfa04c51043f4e14d6940e68f87b55e36e4
Dongkyun Kim <donkim0122@gmail.com>
2024-06-13 14:14:18 +0900

    Update: use gameStateVerifier for end game verification Update: remove timestamp from payload as gsv method handles it

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,34 +1,38 @@
 import { reinitialize } from "../libs/game-state-manager.js";
-import { getStage } from "../models/stage.model.js";
+import { getCurrentStage, getStage } from "../models/stage.model.js";
+import gsv from "../libs/game-state-verifier.js";
 
-export const gameStart = (uuid, payload) => {
-  return reinitialize(uuid, payload.timestamp);
+export const gameStart = (userId, payload) => {
+  return reinitialize(userId, payload.timestamp);
 };
 
-export const gameEnd = (uuid, payload) => {
-  const { timestamp: gameEndTime, score } = payload;
-  const stages = getStage(uuid);
-  if (!stages.length) {
-    return { status: "fail", message: "No stages found for the user." };
-  }
+export const gameEnd = (userId, payload) => {
+  const { score } = payload;
 
-  let totalScore = 0;
-  stages.forEach((stage, index) => {
-    let stageEndTime;
-    if (index === stages.length - 1) {
-      stageEndTime = gameEndTime;
-    } else {
-      stageEndTime = stages[index + 1].timestamp;
-    }
+  // const stages = getStage(userId);
+  // if (!stages.length) {
+  //   return { status: "fail", message: "No stages found for the user." };
+  // }
+  const currentStage = gsv.userCurrentStageVerificationEx(userId);
 
-    const stageDuration = (stageEndTime - stage.timestamp) / 1000;
-    totalScore += stageDuration; // 초당 1점
-  });
+  // let totalScore = 0;
+  // stages.forEach((stage, index) => {
+  //   let stageEndTime;
+  //   if (index === stages.length - 1) {
+  //     stageEndTime = gameEndTime;
+  //   } else {
+  //     stageEndTime = stages[index + 1].timestamp;
+  //   }
+
+  //   const stageDuration = (stageEndTime - stage.timestamp) / 1000;
+  //   totalScore += stageDuration; // 초당 1점
+  // });
 
   // 점수와 타임스탬프 검증
-  if (Math.abs(score - totalScore) > 5) {
-    return { status: "fail", message: "Score verification failed." };
-  }
+  const finalScore = gsv.scoreVerification(userId, currentStage, score);
+  // if (Math.abs(score - totalScore) > 5) {
+  //   return { status: "fail", message: "Score verification failed." };
+  // }
 
   // DB가 있다면 여기서 저장
 
