1bb61d5d05eeeaa79b9495d88ba5a6e0db607906
Dongkyun Kim <donkim0122@gmail.com>
2024-06-14 01:09:22 +0900

    Update: 'gameEnd' checks for highscore, if true, add broadcast key to the return obj.

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,4 +1,4 @@
-import { reinitialize } from "../libs/game-state-manager.js";
+import { reinitialize, checkHighscore } from "../libs/game-state-manager.js";
 import gsv from "../libs/game-state-verifier.js";
 
 export const gameStart = async (uuid, payload) => {
@@ -8,32 +8,14 @@ export const gameStart = async (uuid, payload) => {
 export const gameEnd = async (userId, payload) => {
   const { score } = payload;
 
-  // const stages = getStage(userId);
-  // if (!stages.length) {
-  //   return { status: "fail", message: "No stages found for the user." };
-  // }
+  // verification
   const currentStage = await gsv.userCurrentStageVerificationEx(userId);
-
-  // let totalScore = 0;
-  // stages.forEach((stage, index) => {
-  //   let stageEndTime;
-  //   if (index === stages.length - 1) {
-  //     stageEndTime = gameEndTime;
-  //   } else {
-  //     stageEndTime = stages[index + 1].timestamp;
-  //   }
-
-  //   const stageDuration = (stageEndTime - stage.timestamp) / 1000;
-  //   totalScore += stageDuration; // 초당 1점
-  // });
-
-  // 점수와 타임스탬프 검증
   const deltaScore = gsv.scoreVerification(userId, currentStage, score);
-  // if (Math.abs(score - totalScore) > 5) {
-  //   return { status: "fail", message: "Score verification failed." };
-  // }
 
-  // DB가 있다면 여기서 저장
+  const ret = { status: "success", message: "Game ended", payload: { score } };
 
-  return { status: "success", message: "Game ended", payload: { score } };
+  // highscore 갱신
+  if (await checkHighscore(userId, score)) ret.broadcast = { userId, highscore: score };
+
+  return ret;
 };
