ad176954e63aef582ef8f5115023233799e1b5b0
Dongkyun Kim <donkim0122@gmail.com>
2024-06-14 15:44:18 +0900

    Update: add SPAWN_RATE_TOLERANCE to allow slight difference in item spawn elapsed time

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const CLIENT_VERSION = ["1.0.0", "1.0.1", "1.1.0"];
 export const SCORE_ERROR_TOLERANCE = 5;
+export const SPAWN_RATE_TOLERANCE = 100;
 export const INTERVAL_MIN = 0;
 export const INTERVAL_MAX = 16000;
