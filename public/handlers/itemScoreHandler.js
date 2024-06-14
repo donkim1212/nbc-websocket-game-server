b7af7655d4684d0fbfb7fe4b6db597c9003f3d95
Dongkyun Kim <donkim0122@gmail.com>
2024-06-14 16:18:36 +0900

    Update: disable itemScoreHandler as it is no longer used

--- public/handlers/itemScoreHandler.js
+++ public/handlers/itemScoreHandler.js
@@ -1,8 +1,8 @@
 import { score as gameScore } from "../index.js";
 
 const itemScoreHandler = (payload, score) => {
-  const { score: itemScore } = payload;
-  gameScore.addScore(itemScore);
+  // const { score: itemScore } = payload;
+  // gameScore.addScore(itemScore);
 };
 
 export default itemScoreHandler;
